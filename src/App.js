a93af72d459011a5c6ed7cef63a27a31665a398b
RomanKulyukov <zapryagli@gmail.com>
2022-12-17 23:35:39 +0300

    G

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./App.css";
 import { connect } from "react-redux";
 class App extends Component {
   render() {
-    console.log(this.props);
     return (
       <div className="App">
         <div>{this.props.counter}</div>
+        <button onClick={this.props.onAdd}>Добавить</button>
+        <button onClick={this.props.onSub}>Убавить</button>
+
+        <button onClick={() => this.props.onAddNum(15)}>Добавить 15</button>
+        <button onClick={() => this.props.onSubNum(17)}>Убавить 17</button>
       </div>
     );
   }
@@ -17,4 +21,13 @@ function mapStateToProps(state) {
     counter: state.counter,
   };
 }
-export default connect(mapStateToProps)(App);
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onAdd: () => dispatch({ type: "ADD" }),
+    onSub: () => dispatch({ type: "SUB" }),
+    onAddNum: (num) => dispatch({ type: "ADDNUM", payload: num }),
+    onSubNum: (num) => dispatch({ type: "SUBNUM", payload: num }),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,5 +3,23 @@ const initState = {
 };
 
 export default function rootReducer(state = initState, action) {
+  switch (action.type) {
+    case "ADD":
+      return {
+        counter: state.counter + 1,
+      };
+    case "SUB":
+      return {
+        counter: state.counter - 1,
+      };
+    case "ADDNUM":
+      return {
+        counter: state.counter + action.payload,
+      };
+    case "SUBNUM":
+      return {
+        counter: state.counter - action.payload,
+      };
+  }
   return state;
 }
