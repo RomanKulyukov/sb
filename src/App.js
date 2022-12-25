adff0acfbcb6a045d5a5be6985c10405621ea7f7
RomanKulyukov <zapryagli@gmail.com>
2022-12-25 22:11:56 +0300

    nate suki

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div>{this.props.counter}</div>
-        <button onClick={this.props.onAdd}>Добавить</button>
-        <button onClick={this.props.onSub}>Убавить</button>
-
-        <button onClick={() => this.props.onAddNum(15)}>Добавить 15</button>
-        <button onClick={() => this.props.onSubNum(17)}>Убавить 17</button>
-      </div>
-    );
-  }
+function App(props) {
+  return <div>TEST</div>;
 }
-
-function mapStateToProps(state) {
-  return {
-    counter: state.counter,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    onAdd: () => dispatch({ type: "ADD" }),
-    onSub: () => dispatch({ type: "SUB" }),
-    onAddNum: (num) => dispatch({ type: "ADDNUM", payload: num }),
-    onSubNum: (num) => dispatch({ type: "SUBNUM", payload: num }),
-  };
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "../src/redux/rootReducer";
-const store = createStore(rootReducer);
-const app = (
-  <Provider store={store}>
-    <App></App>
-  </Provider>
-);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(app);
+root.render(<App></App>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
