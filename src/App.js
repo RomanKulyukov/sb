95ca1edf0af883c0876bcafe90fe66cb8b495e48
RomanKulyukov <zapryagli@gmail.com>
2022-12-26 22:10:59 +0300

    ds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+  const [mouse, setMouse] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${value}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [value]);
+  useEffect(() => {
+    window.addEventListener("mousemove", (event) => {
+      setMouse({ x: event.clientX, y: event.clientY });
+    });
+  });
+  return (
+    <div>
+      <div>{value == "" ? "Тут будет что-то" : value}</div>
+      <button
+        onClick={() => {
+          setValue("Users");
+        }}
+      >
+        Пользователи
+      </button>
+      <button
+        onClick={() => {
+          setValue("Todos");
+        }}
+      >
+        Todo
+      </button>
+      <button
+        onClick={() => {
+          setValue("Posts");
+        }}
+      >
+        Посты
+      </button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(mouse, null, 2)}</pre>
+    </div>
+  );
 }
 export default App;
