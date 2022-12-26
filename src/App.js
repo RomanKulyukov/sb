2c41db2f67c669ed4adb0cd57768e141c9da5064
RomanKulyukov <zapryagli@gmail.com>
2022-12-26 16:33:21 +0300

    use

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-  //////////////
-
-  const [counter, changeCounter] = useState(0);
-  const [obj, objState] = useState({ text: "aloha", age: "26" });
-  function older() {
-    objState((prev) => {
-      return { ...prev, age: +obj.age + 2 };
-    });
-  }
-  //////////////
-
-  const [parameter] = useState(10);
-  function multiple() {
-    changeCounter(counter + 20);
-
-    changeCounter((prevStep) => prevStep + 20);
-  }
-  //////////////
-
-  const [cm, cmState] = useState(0);
-
-  return (
-    <div>
-      <div>Counter: {counter}</div>
-      <button onClick={() => changeCounter(counter + parameter)}>+</button>
-      <button onClick={() => changeCounter(counter - parameter)}>-</button>
-      <button onClick={multiple}>+40</button>
-      <div>{`${obj.text} i'm ${obj.age} years old`}</div>
-      <button onClick={older}>make me older</button>
-      <div>{cm}</div>
-      <button></button>
-    </div>
-  );
+  return <div></div>;
 }
 export default App;
